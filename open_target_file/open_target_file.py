e80f9c6ce7d249feb40c930e99313a9c910a247c
aidwao <1986263237@qq.com>
2026-01-29 17:56:22 +0800

    new open target folder

--- open_target_file/open_target_file.py
+++ open_target_file/open_target_file.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 def normalize_path(path):
 	import os
@@ -16,4 +17,17 @@ def open_file(file_path):
 	print(f"打开文件: {file_path} {exit_code=}")
 
 if __name__ == "__main__":
-	open_file(r"G:\workspace\test_ugs_sync\UnrealEngine\UE5.sln")
+	parser = argparse.ArgumentParser(description='Open a file')
+	parser.add_argument('args', type=str, nargs='*', help='all args')
+	args = parser.parse_args()
+	print(f"args: {args}")
+	final_args = args.args
+	print(f"final_args: {final_args}")
+	path = r"G:\workspace\test_ugs_sync\UnrealEngine\UE5.sln"
+	folder_path_list = path.split('\\')[:-1]
+	folder_path = '\\'.join(folder_path_list)
+	if len(final_args) > 0:
+		if ("f" in final_args):
+			open_file(folder_path)
+	else:
+		open_file(r"G:\workspace\test_ugs_sync\UnrealEngine\UE5.sln")
